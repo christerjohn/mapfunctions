71fdf0a6f5b18a23b9b1ee736f8f51f5e9c13c21
ivanisrael <ivanisrael.hernaez@gmail.com>
2016-04-03 14:57:48 +0800

    Commit

---
+++ Template/templating/resources/views/layouts/partials/sidebar.php
@@ -0,0 +1,37 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar" style="height: auto;">
+      <!-- Sidebar user panel -->
+      
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu">
+            <li class="header">NAVIGATION</li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu" style="display: none;">
+                    <li class=""><a href="#"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
+                    <li><a href="#"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+                </ul>
+            </li>
+            <li><a href="#"><i class='fa fa-users'></i> <span>Users</span></a></li>
+            <li><a href="#"><i class='fa fa-university'></i> <span>Pronvinces</span></a></li>
+            <li><a href="#"><i class='fa fa-legal'></i> <span>Barangays</span></a></li>
+            <li><a href="#"><i class='fa fa-home '></i> <span>Households</span></a></li>
+            <li><a href="#"><i class='fa fa-bar-chart'></i> <span>Reports</span></a></li>
+            <li class="treeview">
+                <a href="#">
+                    <i class='fa fa-map-marker'></i> <span>Maps</span> <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu" style="display: none;">
+                    <li class=""><a href="#"><i class="fa fa-circle-o"></i> Population</a></li>
+                    <li><a href="#"><i class="fa fa-circle-o"></i> Flood Maps</a></li>
+                </ul>
+            </li>
+
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
---
+++ Template/templating/resources/views/pages/maps/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+
+
+<?php
+    include '../../layouts/partials/htmlheader.php';
+?>
+
+<body class="skin-blue sidebar-mini wysihtml5-supported">
+    <div class="wrapper">
+
+        <?php
+            include '../../layouts/partials/mainheader.php';
+            include '../../layouts/partials/sidebar.php';
+        ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper" style="min-height: 858px;">
+            <!-- Your Page Content Here /////////////////////////////////////////// -->
+
+<!-- ////////////////////////////// DARI KA EDIT UG SUGOD ////////////////////////////////////////// -->
+
+
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+          <h1>
+            Blank page
+            <small>This is a sample page</small>
+          </h1>
+          <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+            <li><a href="#">Blank Page</a></li>
+          </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+          <!-- Default box -->
+          <div class="box">
+            <div class="box-header with-border">
+              <h3 class="box-title">Title</h3>
+              <div class="box-tools pull-right">
+                
+                
+              </div>
+            </div>
+            <div class="box-body">
+              Start creating your amazing application!
+            </div><!-- /.box-body -->
+            <div class="box-footer">
+              Footer
+            </div><!-- /.box-footer-->
+          </div><!-- /.box -->
+
+        </section><!-- /.content -->
+
+
+
+<!-- ////////////////////////////// DARI RA TAMAN KA EDIT ////////////////////////////////////////// -->
+
+
+
+        </div><!-- /.content-wrapper /////////////////////////////////////////// -->
+        <?php
+            include '../../layouts/partials/footer.php';
+        ?>
+    </div><!-- ./wrapper -->
+    <?php
+        include '../../layouts/partials/scripts.php';
+    ?>
+
+</body>
+</html>
